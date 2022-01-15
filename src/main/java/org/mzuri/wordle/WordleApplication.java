293c06443ba0eecf0c09c534d140481bd09cb2bd
jbeaken <j.beaken@elsevier.com>
2022-01-15 15:29:32 +0000

    Args interperation

--- src/main/java/org/mzuri/wordle/WordleApplication.java
+++ src/main/java/org/mzuri/wordle/WordleApplication.java
@@ -8,16 +8,17 @@ class WordleApplication {
 
     public static void main(String[] args) {
 
-        if(args.length !=1 || args[0].length() != 5) {
-            log.error("Usage : WordleApplication {wordToGuess} of exactly 5 letters length");
+        if(args.length > 2 || args[0].length() !=5 || (args.length == 2 && args[1].length() !=5)) {
+            log.error("Usage : WordleApplication {wordToGuess} {seedGuess} of exactly 5 letters length");
             System.exit(1);
         }
 
         String wordToGuess = args[0];
+        String seedGuess = (args.length == 2 ? args[1] : "roate");
 
         log.info("Hello from Wordle Application, booting up bot");
 
-        WordleBot wordleBot = new WordleBot( wordToGuess );
+        WordleBot wordleBot = new WordleBot( wordToGuess, seedGuess );
 
         log.info("Bot ready for wordle action");
 
